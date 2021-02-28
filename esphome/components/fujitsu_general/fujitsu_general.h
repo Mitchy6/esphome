0ab1769edea7d41be5409a42d58ad33d23be6bd8
Alex Richards <alex.github@gmail.com>
2021-02-28 16:03:05 +1300

    Added receive for Fujitsu ACs

--- esphome/components/fujitsu_general/fujitsu_general.h
+++ esphome/components/fujitsu_general/fujitsu_general.h
@@ -1,15 +1,23 @@
 #pragma once
 
+#include "esphome/core/log.h"
 #include "esphome/core/component.h"
 #include "esphome/core/automation.h"
 #include "esphome/components/climate_ir/climate_ir.h"
 
+
 namespace esphome {
 namespace fujitsu_general {
 
+const uint8_t FUJITSU_GENERAL_TEMP_MIN = 16;  // Celsius // TODO 16 for heating, 18 for cooling, unsupported in ESPH
+const uint8_t FUJITSU_GENERAL_TEMP_MAX = 30;  // Celsius
+
 class FujitsuGeneralClimate : public climate_ir::ClimateIR {
  public:
-  FujitsuGeneralClimate();
+  FujitsuGeneralClimate() : ClimateIR(
+      FUJITSU_GENERAL_TEMP_MIN, FUJITSU_GENERAL_TEMP_MAX, 1.0f, true, true,
+      {climate::CLIMATE_FAN_AUTO, climate::CLIMATE_FAN_LOW, climate::CLIMATE_FAN_MEDIUM, climate::CLIMATE_FAN_HIGH},
+      {climate::CLIMATE_SWING_OFF, climate::CLIMATE_SWING_VERTICAL, climate::CLIMATE_SWING_HORIZONTAL, climate::CLIMATE_SWING_BOTH}) {}
 
  protected:
   /// Transmit via IR the state of this climate controller.
@@ -17,6 +25,19 @@ class FujitsuGeneralClimate : public climate_ir::ClimateIR {
   /// Transmit via IR power off command.
   void transmit_off_();
 
+  /// Parse incomming message
+  bool on_receive(remote_base::RemoteReceiveData data) override;
+
+  /// Transmit message as IR pulses
+  void transmit_(const uint8_t message[], const uint8_t length);
+  
+  /// Calculate checksum for a state message
+  uint8_t checksum_state_(const uint8_t message[]);
+
+  /// Calculate cecksum for a util message
+  uint8_t checksum_util_(const uint8_t message[]);
+
+  // true if currently on - fujitsus transmit an on flag on when the remote moves from off to on
   bool power_{false};
 };
 
